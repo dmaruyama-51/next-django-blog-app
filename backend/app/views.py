9befc06721db1df8f856be412f19a0680a4b5650
キカガク <kikagaku@MacBook-Pro-10.local>
2021-12-26 00:35:40 +0900

    add frontend

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,14 +1,13 @@
-from django.db.models.query import QuerySet
-from django.shortcuts import render
-from rest_framework import generics 
+from rest_framework import generics
 from .serializers import PostSerializer
 from .models import Post
 
-# Create your views here.
+
 class PostView(generics.ListAPIView):
-    queryset = Post.objects.all() 
-    serializer_class = PostSerializer 
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
 
 class PostDetailView(generics.RetrieveAPIView):
-    QuerySet = Post.objects.all()
-    serializer_class = PostSerializer 
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
